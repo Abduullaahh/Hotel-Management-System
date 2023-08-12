d422308cbabd1929406c5299f25088d7d3a72fa7
Abdullah Tahir <108390173+Abduullaahh@users.noreply.github.com>
2023-08-12 20:17:10 +0500

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.set('view engine', 'jade');
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://mirage-hotel.netlify.app");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
